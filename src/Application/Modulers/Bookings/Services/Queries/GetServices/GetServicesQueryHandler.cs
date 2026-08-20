e907808405e14d11d57465c3837918acaeb241bf
ASUS <ASUS@AWADAZ>
2026-08-20 14:43:50 +0300

    Application:  (Services) Fixed GetAllServices UseCases

--- src/Application/Modulers/Bookings/Services/Queries/GetServices/GetServicesQueryHandler.cs
+++ src/Application/Modulers/Bookings/Services/Queries/GetServices/GetServicesQueryHandler.cs
@@ -12,6 +12,7 @@ internal sealed class GetServicesQueryHandler(IApplicationDbContext context)
             .AsNoTracking()
             .Select(s => new ServiceDto
             {
+                Id = s.Id,
                 ServiceName = s.ServiceName,
                 Description = s.Description,
                 ServiceType = s.ServiceType,
